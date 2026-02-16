9a454cfbdeaf7159e2cd48ebd7bcd9ca5c19229d
Devani-Karthikeyan <devaniselva31@gmail.com>
2026-02-16 15:23:43 +0530

    Initial frontend setup with React and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-white text-3xl font-bold">
+        Tailwind v4 is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
